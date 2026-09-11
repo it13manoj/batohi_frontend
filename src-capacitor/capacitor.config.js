452ee2059b600158398d5cecda8a9ed299846986
Manoj Sharma <manoj.k@smbgroup.co.in>
2026-09-11 18:01:19 +0530

    update

--- src-capacitor/capacitor.config.js
+++ src-capacitor/capacitor.config.js
@@ -1,11 +1,15 @@
-/** @type {import('@capacitor/cli').CapacitorConfig} */
-const config = {
+const { defineCapacitorConfig } = require('@quasar/app-vite/capacitor');
+
+module.exports = defineCapacitorConfig({
   appId: 'com.batohi.app',
   appName: 'Batohi App',
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    CapacitorHttp: {
+      enabled: true // Bypasses browser WebView CORS restrictions on native Android
+    }
   }
-};
-
-module.exports = config;
+});
